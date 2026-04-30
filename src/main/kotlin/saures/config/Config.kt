0da3d7fe61a35559c1704c5ac5323f071e9e6bd6
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-04-30 22:00:29 +0300

    Переименованы переменные окружения для подключения к базе данных
    SAURES_DB_URL, SAURES_DB_USER и SAURES_DB_PASSWORD заменены на DB_URL, DB_USER и DB_PASSWORD соответственно.

--- src/main/kotlin/saures/config/Config.kt
+++ src/main/kotlin/saures/config/Config.kt
@@ -18,9 +18,9 @@ data class Config(
             return Config(
                 email       = email,
                 password    = password,
-                dbUrl       = System.getenv("SAURES_DB_URL")      ?: "jdbc:postgresql://localhost:5456/saures",
-                dbUser      = System.getenv("SAURES_DB_USER")     ?: "saures",
-                dbPassword  = System.getenv("SAURES_DB_PASSWORD") ?: "saures",
+                dbUrl       = System.getenv("DB_URL")      ?: "jdbc:postgresql://localhost:5456/saures",
+                dbUser      = System.getenv("DB_USER")     ?: "saures",
+                dbPassword  = System.getenv("DB_PASSWORD") ?: "saures",
                 serverPort  = System.getenv("SERVER_PORT")?.toIntOrNull() ?: 8080
             )
         }
